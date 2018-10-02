f3a20354bb301843e0b681b2da19efb74dabccb1
BullDog57Rus <m.salo@innopolis.ru>
2018-10-02 20:16:49 +0300

    Maxim is very pidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { BLE } from "@ionic-native/ble";
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {BLE} from "@ionic-native/ble";
 
-import { AboutPage } from '../pages/about/about';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { SettingsPage } from "../pages/settings/settings";
+import {AboutPage} from '../pages/about/about';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import {SettingsPage} from "../pages/settings/settings";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
@@ -39,4 +39,5 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     BLE
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,7 @@
 import {Component, NgZone} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { BLE } from "@ionic-native/ble";
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {BLE} from "@ionic-native/ble";
+import {QRScanner, QRScannerStatus} from '@ionic-native/qr-scanner';
 
 /**
  * Generated class for the SettingsPage page.
@@ -22,7 +23,8 @@ export class SettingsPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public ngZone: NgZone,
-              public ble: BLE) {
+              public ble: BLE,
+              public qrScanner: QRScanner) {
   }
 
   ionViewDidEnter() {
@@ -41,6 +43,24 @@ export class SettingsPage {
     setTimeout(this.setStatus.bind(this), 5000, 'Scan complete');
   }
 
+  scanQrCode() {
+    this.qrScanner.prepare().then((status: QRScannerStatus) => {
+      if (status.authorized) {
+        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+          console.log("Scanned QR", text);
+
+          this.qrScanner.hide();
+          scanSub.unsubscribe();
+        });
+      } else if (status.denied) {
+        this.setStatus("Camera permission was permanently denied");
+        this.qrScanner.openSettings();
+      } else {
+        this.setStatus("Something went wrong, please, try again later");
+      }
+    }).catch((e: any) => console.log('Error is', e));
+  }
+
   onDeviceDiscovered(device) {
     console.log('Discovered ' + JSON.stringify(device, null, 2));
     this.ngZone.run(() => {
@@ -50,7 +70,7 @@ export class SettingsPage {
 
   // If location permission is denied, you'll end up here
   scanError(error) {
-    this.setStatus('Error ' + error);
+    this.setStatus('Error: ' + error);
   }
 
   setStatus(message) {
